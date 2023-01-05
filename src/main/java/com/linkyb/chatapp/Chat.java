1491e81d403b9e8d1b8ccf99662542c4fe5fcf1b
DongwookKim0823 <dwkim0823@naver.com>
2023-01-05 20:54:06 +0900

    Repository 생성

---
+++ src/main/java/com/linkyb/chatapp/Chat.java
@@ -0,0 +1,20 @@
+package com.linkyb.chatapp;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document(collation = "chat")
+public class Chat {
+
+    @Id
+    private String id;
+    private String msg;
+    private String sender; //보내는 사람
+    private String receiver; //받는 사람
+
+    private LocalDateTime createdAt;
+}
