8fb0de372ea8fb5cbdaf5eb136c047c44f4941ce
DongwookKim0823 <dwkim0823@naver.com>
2023-01-06 03:09:36 +0900

    방 별로 채팅하는 기능

--- src/main/java/com/linkyb/chatapp/ChatController.java
+++ src/main/java/com/linkyb/chatapp/ChatController.java
@@ -15,15 +15,26 @@ public class ChatController {
 
     private final ChatRepository chatRepository;
 
+    //일대일 대화 시에 사용
+    @CrossOrigin
     @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Chat> getMsgs(@PathVariable String sender, @PathVariable String receiver) {
         return chatRepository.mFindBySender(sender, receiver)
                 .subscribeOn(Schedulers.boundedElastic());
     }
 
+    //방 별로 채팅
+    @CrossOrigin
+    @GetMapping(value = "/chat/roomNum/{roomNum}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> findByRoomNum(@PathVariable Integer roomNum) {
+        return chatRepository.nFindByRoomNum(roomNum)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @CrossOrigin
     @PostMapping("/chat")
     public Mono<Chat> setMsg(@RequestBody Chat chat) {
         chat.setCreatedAt(LocalDateTime.now());
-        return chatRepository.save(chat);
+        return chatRepository.save(chat); // Object를 리턴하면 자동으로 JSON 변환(by MessageConverter)
     }
 }
--- src/main/java/com/linkyb/chatapp/ChatRepository.java
+++ src/main/java/com/linkyb/chatapp/ChatRepository.java
@@ -10,4 +10,8 @@ public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
     @Tailable // 커서를 안닫고 계속 유지한다.
     @Query("{sender:?0, receiver:?1}")
     Flux<Chat> mFindBySender(String sender, String receiver); // Flux(흐름) response를 유지하면서 데이터를 계속 흘려보내기
+
+    @Tailable
+    @Query("{roomNum:?0}")
+    Flux<Chat> nFindByRoomNum(Integer roomNum);
 }
